bd5b68f5bb7881f6d81b6b53c49fd2fa844f7efb
Daniel Nemergut <dpnemergut@gmail.com>
2014-09-10 18:27:55 -0400

    Skeleton of assignment 1

--- Assignment1/script.rb
+++ Assignment1/script.rb
@@ -1,1 +1,22 @@
-#Assignment 1
+# Assignment 1
+# Collaborators:
+#  Daniel Nemergut
+#  Garrett
+#  Clint
+
+# Define a method sum which takes an array of integers as an argument
+# and returns the sum of its elements. For an empty array it should
+# return zero.
+
+
+# Define a method max_2_sum which takes an array of integers as an argument
+# and returns the sum of its two largest elements. For an empty array it
+# should return zero. For an array with just one element, it should return
+# that element.
+
+
+# Define a method sum_to_n? which takes an array of integers and an additional
+# integer, n, as arguments and returns true if any two distinct elements in
+# the array of integers sum to n. An empty array or single element array
+# should both return false.
+
